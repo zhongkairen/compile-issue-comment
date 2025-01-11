f4c13a37bc467ae8496fcdd5b4b391f9a8281d6b
Zhongkai Ren <zhongkai.ren@unity3d.com>
2025-01-11 23:35:20 +0200

    Refactor repository and discussion classes

--- scripts/aggregate_comments.py
+++ scripts/aggregate_comments.py
@@ -3,185 +3,164 @@ from github import Github
 import requests
 
 
-def get_issues_and_comments(repo, label, comment_prefix):
-    issues = repo.get_issues(state="open", labels=[label])
-    aggregated_content = ""
+class IssueComment:
+    """Data class for GitHub comments."""
 
-    for issue in issues:
-        relevant_comments = [
-            comment for comment in issue.get_comments() if comment.body.startswith(comment_prefix)
+    def __init__(self, issue, comment_prefix):
+        self.issue = issue
+        self.comment_prefix = comment_prefix
+
+    def __str__(self):
+        aggregated_content = f"**{self.issue.title}** (#{self.issue.number}) - "
+        comments = [comment.body.replace(
+            self.comment_prefix, "").lstrip() for comment in self.comments]
+        aggregated_content += " ".join(comments)
+        return aggregated_content
+
+    @property
+    def comments(self):
+        return [
+            comment for comment in self.issue.get_comments() if comment.body.startswith(self.comment_prefix)
         ]
-        if relevant_comments:
-            aggregated_content += f"### Issue #{issue.number}: {issue.title} - "
-            comments = [comment.body.replace(
-                comment_prefix, "").lstrip() for comment in relevant_comments]
-            aggregated_content += " ".join(comments)
-            print(f"Aggregated content: '{comments}'")
-
-    return aggregated_content
 
 
-def fetch_discussions(owner, repo_name, graphql_schema, token):
-    """Fetch discussions from the repository using a GraphQL query."""
-    headers = {"Authorization": f"Bearer {token}"}
-    query = {
-        "query": graphql_schema,
-        "variables": {
-            "owner": owner,
-            "repo": repo_name,
-        },
-    }
-    response = requests.post(
-        "https://api.github.com/graphql", json=query, headers=headers
-    )
-    response.raise_for_status()  # Raise an error for HTTP failures
-    result = response.json()
-    print(f"GraphQL Response: {result}")  # Add this line to debug
-    if "data" not in result:
-        raise ValueError(f"Unexpected GraphQL response format: {result}")
-    return result["data"]["repository"]["discussions"]["nodes"]
+class Repository:
+    """Wrapper class for the GitHub repository."""
 
+    def __init__(self, repository, token):
+        g = Github(token)
+        self.repo = g.get_repo(repository)
+        self.owner = self.repo.owner.login
+        self.name = self.repo.name
+        self.token = token
 
-def edit_discussion(discussion_id, new_content, token):
-    """
-    Edit an existing discussion using the GitHub GraphQL API.
-    """
-    mutation = """
-    mutation($discussionId: ID!, $body: String!) {
-        updateDiscussion(input: {discussionId: $discussionId, body: $body}) {
-            discussion {
-                id
-                title
-                body
+    @property
+    def id(self):
+        if not hasattr(self, "_id"):
+            self._id = self.query_id()
+        return self._id
+
+    def get_aggregated_comment(self, label, comment_prefix):
+        issues = self.repo.get_issues(state="open", labels=[label])
+        return "\n".join(str(IssueComment(issue, comment_prefix)) for issue in issues)
+
+    def post(self, query, variables):
+        headers = {"Authorization": f"Bearer {self.token}"}
+        response = requests.post(
+            "https://api.github.com/graphql", json={"query": query, "variables": variables}, headers=headers
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def query_id(self):
+        query = """
+            query($owner: String!, $repo: String!) {
+                repository(owner: $owner, name: $repo) {
+                    id
+                }
             }
-        }
-    }
-    """
-    headers = {"Authorization": f"Bearer {token}"}
-    variables = {"discussionId": discussion_id, "body": new_content}
-    response = requests.post(
-        "https://api.github.com/graphql",
-        json={"query": mutation, "variables": variables},
-        headers=headers,
-    )
-    response.raise_for_status()
-    data = response.json()
+        """
+        headers = {"Authorization": f"Bearer {self.token}"}
+        variables = {"owner": self.owner, "repo": self.name}
+        data = self.post(query, variables)
+        return data["data"]["repository"]["id"]
 
-    # Debug the response
-    print(f"Edit Discussion Response: {data}")
-    if "errors" in data:
-        raise ValueError(f"Failed to update discussion: {data['errors']}")
-    return data["data"]["updateDiscussion"]["discussion"]
-
-
-def create_or_update_discussion(repo, discussion_heading, comment_prefix, content, graphql_schema, owner, repo_name, token, category_id):
-    content = f"# {discussion_heading}\n\n" + content
-    discussion_title = comment_prefix.strip("[]").strip()
-    discussions = fetch_discussions(owner, repo_name, graphql_schema, token)
-
-    # Check for existing discussion
-    for discussion in discussions:
-        if discussion["title"] == discussion_title:
-            # Edit the discussion if found
-            # repo.get_discussion(discussion["id"]).edit(body=content)
-            edit_discussion(discussion["id"], content, token)
-            return
-
-    # If not found, create a new discussion using the GraphQL mutation
-    mutation = """
-    mutation($title: String!, $body: String!, $repoId: ID!, $categoryId: ID!) {
-    createDiscussion(input: {repositoryId: $repoId, categoryId: $categoryId, title: $title, body: $body}) {
-            discussion {
-                id
-                title
-            }
-        }
-    }
-    """
-    # Make sure to fetch the repository ID here
-    repo_id_query = """
-    query($owner: String!, $repo: String!) {
+    def query_discussion_category_ids(self):
+        """
+        Fetch the category ID for a discussion category using the GitHub GraphQL API.
+        """
+        query = """
+        query($owner: String!, $repo: String!) {
         repository(owner: $owner, name: $repo) {
-            id
+            discussionCategories(first: 100) {
+            nodes {
+                id
+                name
+            }
+            }
         }
-    }
-    """
-    # Get repository ID
-    headers = {"Authorization": f"Bearer {token}"}
-    repo_query = {
-        "query": repo_id_query,
-        "variables": {"owner": owner, "repo": repo_name},
-    }
-    repo_response = requests.post(
-        "https://api.github.com/graphql", json=repo_query, headers=headers
-    )
-    repo_response.raise_for_status()
-    repo_data = repo_response.json()
-    repo_id = repo_data["data"]["repository"]["id"]
+        }
+        """
+        variables = {"owner": self.owner, "repo": self.name}
+        data = self.post(query, variables)
 
-    # Now create the discussion using the mutation
-    mutation_query = {
-        "query": mutation,
-        "variables": {
-            "owner": owner,
-            "repo": repo_name,
-            "title": discussion_title,
-            "body": content,
-            "repoId": repo_id,
+        # Extract categories
+        categories = data["data"]["repository"]["discussionCategories"]["nodes"]
+        return {category["name"]: category["id"] for category in categories}
+
+
+class Discussion:
+    """Wrapper class for the GitHub discussion."""
+
+    def __init__(self, repo):
+        self.repo = repo
+        # self._title = None
+        self.category_map = None
+
+    def get_category_id(self, category_name):
+        if not self.category_map:
+            self.category_map = self.repo.query_discussion_category_ids()
+        return self.category_map.get(category_name)
+
+    def get_discussion_id(self, title: str):
+        discussion_schema = """
+            id
+            title
+        """
+        discussions_pages = self.repo.repo.get_discussions(discussion_schema)
+        discussions = list(discussions_pages)
+        for discussion in discussions:
+            if discussion.title == title:
+                return discussion.id
+
+    def edit(self, discussion_id, body):
+        update_discussion_mutation = """
+        mutation($discussionId: ID!, $body: String!) {
+            updateDiscussion(input: {discussionId: $discussionId, body: $body}) {
+                discussion {
+                    id
+                    title
+                    body
+                }
+            }
+        }
+        """
+        variables = {"discussionId": discussion_id, "body": body}
+        data = self.repo.post(update_discussion_mutation, variables)
+
+        if "errors" in data:
+            raise ValueError(f"Failed to update discussion: {data['errors']}")
+        return data["data"]["updateDiscussion"]["discussion"]
+
+    def create(self, category_id, title, body):
+        # If not found, create a new discussion using the GraphQL mutation
+        create_discussion_mutation = """
+        mutation($title: String!, $body: String!, $repoId: ID!, $categoryId: ID!) {
+        createDiscussion(input: {repositoryId: $repoId, categoryId: $categoryId, title: $title, body: $body}) {
+                discussion {
+                    id
+                    title
+                }
+            }
+        }
+        """
+        # Now create the discussion using the mutation
+        mutation_result = self.repo.post(create_discussion_mutation, {
+            "title": title,
+            "body": body,
+            "repoId": self.repo.id,
             "categoryId": category_id,
-        },
-    }
+        })
 
-    # todo: add usage of discussion category
+        # Add this line to debug
+        print(f"Create discussion Response: {mutation_result}")
 
-    mutation_response = requests.post(
-        "https://api.github.com/graphql", json=mutation_query, headers=headers
-    )
-    mutation_response.raise_for_status()
-    mutation_result = mutation_response.json()
-    # Add this line to debug
-    print(f"Create discussion Response: {mutation_result}")
+        if mutation_result.get("errors"):
+            raise ValueError(
+                f"Failed to create discussion: {mutation_result['errors']}")
 
-    if mutation_result.get("errors"):
-        raise ValueError(
-            f"Failed to create discussion: {mutation_result['errors']}")
-
-    print(
-        f"Created discussion with title: {mutation_result['data']['createDiscussion']['discussion']['title']}")
-
-
-def get_category_id(owner, repo_name, category_name, token):
-    """
-    Fetch the category ID for a discussion category using the GitHub GraphQL API.
-    """
-    query = """
-    query($owner: String!, $repo: String!) {
-      repository(owner: $owner, name: $repo) {
-        discussionCategories(first: 100) {
-          nodes {
-            id
-            name
-          }
-        }
-      }
-    }
-    """
-    headers = {"Authorization": f"Bearer {token}"}
-    variables = {"owner": owner, "repo": repo_name}
-    response = requests.post(
-        "https://api.github.com/graphql",
-        json={"query": query, "variables": variables},
-        headers=headers,
-    )
-    response.raise_for_status()
-    data = response.json()
-
-    # Extract categories
-    categories = data["data"]["repository"]["discussionCategories"]["nodes"]
-    for category in categories:
-        if category["name"] == category_name:
-            return category["id"]
-    return None
+        print(
+            f"Created discussion with title: {mutation_result['data']['createDiscussion']['discussion']['title']}")
 
 
 def main():
@@ -200,45 +179,33 @@ def main():
     print(f"DISCUSSION_HEADING: {discussion_heading}")
     print(f"DISCUSSION_CATEGORY: {discussion_category}")
 
-    owner = repository.split("/")[0]  # Extract owner from 'owner/repo'
-    repo_name = repository.split("/")[1]  # Extract repo name from 'owner/re
-
-    # Debug line
-    print(f"Fetching discussions for owner: {owner} and repo: {repo_name}")
-
-    g = Github(token)
-    repo = g.get_repo(repository)
+    repo = Repository(repository, token)
 
     # Fetch category ID if it's provided
-    category_id = None
-    if discussion_category:
-        category_id = get_category_id(
-            owner, repo_name, discussion_category, token)
-        if category_id:
-            print(
-                f"Found discussion category ID: {category_id} for '{discussion_category}'")
-        else:
-            print(f"discussion category '{discussion_category}' not found!")
-            exit(1)
+    discussion = Discussion(repo)
 
-    graphql_schema = """
-    query ($owner: String!, $repo: String!) {
-      repository(owner: $owner, name: $repo) {
-        discussions(first: 100) {
-          nodes {
-            id
-            title
-            body
-          }
-        }
-      }
-    }
-    """
+    category_id = discussion.get_category_id(discussion_category)
 
-    aggregated_content = get_issues_and_comments(
-        repo, label_name, comment_prefix)
-    create_or_update_discussion(
-        repo, discussion_heading, comment_prefix, aggregated_content, graphql_schema, owner, repo_name, token, category_id)
+    if category_id:
+        print(
+            f"Found discussion category ID: {category_id} for '{discussion_category}'")
+    else:
+        print(f"discussion category '{discussion_category}' not found!")
+        exit(1)
+
+    aggregated_content = repo.get_aggregated_comment(
+        label_name, comment_prefix)
+
+    discussion_id = discussion.get_discussion_id(title=discussion_heading)
+    if discussion_id:
+        print(f"Discussion found: {discussion_heading}, {discussion_id}")
+        discussion.edit(discussion_id, body=aggregated_content)
+    else:
+        print(f"Discussion not found: {discussion_heading}")
+        # create new discussion
+        discussion.get_category_id(discussion_category)
+        discussion.create(category_id, title=discussion_heading,
+                          body=aggregated_content)
 
 
 if __name__ == "__main__":
